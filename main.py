738e6ba9e470f85edba86fd2932598b94a98df54
Rohit Sharma <rohitrsh@gmail.com>
2018-10-11 16:57:47 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+import os
+import json
+import random
+from httplib import HTTPException
+from urllib2 import HTTPError, URLError
+
+from flask import Flask, jsonify, make_response, request
+
+APP = Flask(__name__)
+LOG = APP.logger
+
+
+@APP.route('/webhook', methods=['POST'])
+def webhook():
+    req = request.get_json(silent=True, force=True)
+    
+    print("Request:")
+    print(json.dumps(req))
+    jobName=req["queryResult"]["parameters"]["jobName"]
+    os.system('curl -k -X POST https://sebuild.tejdrive.com:12443/job/'+jobName+'/build --user xxxxxxx:xxxxxxxx')
+    #os.system('curl -k -X POST https://sebuild.tejdrive.com:12443/job/Test-Rohit/build --user xxxxxxxx:xxxxxxxx')
+    return jsonify(req)
+
+webhookinput = (webhook)
+
+print webhookinput
+
+if __name__ == '__main__':
+    PORT = 8080
+
+    APP.run(
+        debug=True,
+        port=PORT,
+        host='0.0.0.0'
+    )
